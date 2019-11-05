8a92b495dd3da7de02a9a3b6229de104978903ca
bwilm <32461222+bwilm@users.noreply.github.com>
2019-11-04 20:22:49 -0500

    graphql demo

---
+++ server.js
@@ -0,0 +1,130 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const app = express()
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLInt,
+    GraphQLNonNull
+     
+} = require('graphql')
+
+const authors =[
+    {id: 1, name: 'J.K. Rowling'},
+    {id: 2, name: 'J.R.R. Tolkien' },
+    {id: 3, name: 'Brent Weeks'}
+]
+
+const books = [
+    {id: 1, name: 'Harry Potter and the Chamber of secrets', authorId: 1 },
+    {id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+    {id: 3, name: 'Harry Potter and the Goblet of Fire', authorId: 1 },
+    {id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+    {id: 5, name: 'Beyond the Shadows', authorId: 3 }
+]
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    description: 'This represents the book written by author',
+    fields: () => ({
+        id: {type:  GraphQLNonNull(GraphQLInt)},
+        name: { type:  GraphQLNonNull(GraphQLString)},
+        authorId: {type:  GraphQLNonNull(GraphQLInt)},
+        author: {
+            type: AuthorType,
+            resolve: (book) => {
+                return authors.find(author => author.id === book.authorId )
+            }
+        }
+    })
+})
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'This represents the author of the book',
+    fields: () => ({
+        id: {type:  GraphQLNonNull(GraphQLInt)},
+        name: { type:  GraphQLNonNull(GraphQLString) },
+        books: {type: GraphQLList(BookType),
+        resolve: (author)=>{
+            return  books.filter(book => book.authorId === author.id)
+        }}
+    })
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'Query',
+    description: 'Root Query',
+    fields: () => ({
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'List of books',
+            resolve: () => books
+        },
+        book: {
+            type: BookType,
+            description: 'a single book',
+            args: {
+            id:{ type: GraphQLInt}
+            },
+            resolve: (parent, args) => books.find(book => book.id === args.id)
+        },
+        authors: {
+            type: new GraphQLList(AuthorType),
+            description: 'List of Authors',
+            resolve: () => authors
+        },
+        author: {
+            type: AuthorType,
+            description: 'single author',
+            args:{
+                id: { type: GraphQLInt}
+            },
+            resolve: (parent, args) => authors.find(author => author.id === args.id)
+        }
+    })
+})
+
+const RootMutationType = new GraphQLObjectType({
+    name: 'Mutation',
+    description: 'root mutations',
+    fields: () => ({
+        addBook: { 
+            type: BookType,
+            description: 'adding a book',
+            args: {
+                name: { type: GraphQLNonNull(GraphQLString)},
+                authorId: { type: GraphQLNonNull(GraphQLInt)}
+            },
+            resolve: (parent, args) => {
+                const book = { id: books.length + 1, name: args.name, authorId: args.authorId}
+                books.push(book)
+                return book
+            },
+                 addAuthor: { 
+                type: AuthorType,
+                description: 'adding a author',
+                args: {
+                    name: { type: GraphQLNonNull(GraphQLString)}
+                },
+                resolve: (parent, args) => {
+                    const author = { id: authors.length + 1, name: args.name}
+                    authors.push(author)
+                    return author
+                }
+            }
+        }
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: RootQueryType,
+    mutation: RootMutationType
+})
+
+app.use('/graphql', expressGraphQL({
+    schema: schema,
+    graphiql: true
+}))
+app.listen(5000., () => console.log('server is running'))
